627c548c66a3e3f3c6697251c5e779b7b6fd4a1d
NiklasCarlos1994 <nhaarbeck@yahoo.de>
2022-05-01 14:44:20 +0200

    Add files via upload

--- mein_externes_Js.js
+++ mein_externes_Js.js
@@ -25,19 +25,83 @@ arbeitstarif:[70,40,40,40,20]
 
 window.onload=function(){
   
+
+
+    var vonDate = document.getElementById("Von");
+    var bisDate = document.getElementById("Bis");
+
 var saison = document.getElementById("saison");
 var intern_extern = document.getElementById("intern_extern");
 var anzahl_urlaubstage = document.getElementById("anzahl_urlaubstage");
 var anzahl_arbeitstage = document.getElementById("anzahl_arbeitstage");
 var anzahl_bonuspunkte = document.getElementById("anzahl_bonuspunkte");
+
+
+
+//result var for output
 var result = document.getElementById("result")
+var result_sum_anz_utage= document.getElementById("sum_anz_utage");
+var result_anzahl_arbeitstage = document.getElementById("sum_anz_atage");
+var result_anzahl_bonuspunkte = document.getElementById("sum_anz_bpkt");
+var result_sum_days = document.getElementById("sum_days");
+var result_sum_price = document.getElementById("sum_price");
+console.log(result);
+console.log(result_sum_days);
 
 
+//add eventlistener
 saison.addEventListener("change",berechne_preis);
 intern_extern.addEventListener("change",berechne_preis);
 anzahl_arbeitstage.addEventListener("input", berechne_preis);
 anzahl_urlaubstage.addEventListener("input", berechne_preis);
 anzahl_bonuspunkte.addEventListener("input", berechne_preis);
+
+vonDate.addEventListener("input", berechne_Tage);
+bisDate.addEventListener("input", berechne_Tage);
+
+
+
+
+
+
+}
+
+
+function berechne_Tage(){
+
+    /*
+    function isValidDate(d) {
+  return d instanceof Date && !isNaN(d);
+
+werden nicht erkannt waurm??
+  console.log(vonDate.value);
+console.log(bisDate.value);
+} */
+
+
+
+
+    console.log(new Date(document.getElementById("Von").value));
+    console.log(new Date(document.getElementById("Bis").value));
+    console.log("von " + !isNaN(new Date(document.getElementById("Von").value)));
+    console.log( "bis " + !isNaN(new Date(document.getElementById("Bis").value)));
+ 
+
+    console.log("vor if")
+if(!isNaN(new Date(document.getElementById("Von").value)) && !isNaN(new Date(document.getElementById("Bis").value))){
+
+    var anreise = new Date(document.getElementById("Von").value);
+    var abreise = new Date(document.getElementById("Bis").value);
+
+    console.log("rechne");
+    console.log( (abreise - anreise)/1000/60/60/24) 
+var res_tage = (abreise - anreise)/1000/60/60/24;
+
+document.getElementById("res_tage").innerHTML = "Deine Anzahl an Tagen beträgt: " + res_tage
+
+}
+
+
 
 }
 
@@ -55,12 +119,29 @@ var preis_arbeitstage = (csv_preismodell.arbeitstarif[tmp_index] )*anzahl_arbeit
 
 var add = preis_arbeitstage + preis_urlaubstage - 20*anzahl_bonuspunkte.value
 
+
+
 if(add>0){
-result.innerHTML = "Die Summe aus Arbeits- und Urlaubstagen beträgt " + add + " euro";
+
+    document.getElementById("sum_anz_utage").innerHTML = preis_urlaubstage;
+    document.getElementById("sum_anz_atage").innerHTML = preis_arbeitstage;
+    document.getElementById("sum_anz_bpkt").innerHTML = (-1)*20*anzahl_bonuspunkte.value;
+    document.getElementById("sum_days").innerHTML =  (parseInt(anzahl_urlaubstage.value)+parseInt(anzahl_arbeitstage.value));
+    document.getElementById("sum_price").innerHTML = add;
+
+result.innerHTML = "Die Summe aus Arbeits- und Urlaubstagen beträgt " + add + " €" ;
+
 
 	
 }else{
-    result.innerHTML = "Die Summe aus Arbeits- und Urlaubstagen beträgt " + 0 + " euro";
+    result.innerHTML = "Die Summe aus Arbeits- und Urlaubstagen beträgt " + 0 + " €";
+
+    document.getElementById("sum_anz_bpkt").innerHTML = (-1)*20*anzahl_bonuspunkte.value;
+    document.getElementById("sum_price").innerHTML = 0;
+    document.getElementById("sum_anz_utage").innerHTML = 0;
+    document.getElementById("sum_anz_atage").innerHTML = 0;
+    document.getElementById("sum_days").innerHTML = 0;
+
 
 }
 }
