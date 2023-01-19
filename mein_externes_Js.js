5e4c0e64191112670fdc24f99c4514dd482c71f7
NiklasCarlos1994 <nhaarbeck@yahoo.de>
2023-01-19 22:22:32 +0100

    ohne arbeit

--- mein_externes_Js.js
+++ mein_externes_Js.js
@@ -19,7 +19,7 @@ intern:[100,70,70,70,50 ],
 	
 extern:[110,80,80,80,60],
 	
-arbeitstarif:[70,40,40,40,20]
+//arbeitstarif:[70,40,40,40,20]
 	
 }
 
@@ -33,7 +33,7 @@ window.onload=function(){
 var saison = document.getElementById("saison");
 var intern_extern = document.getElementById("intern_extern");
 var anzahl_urlaubstage = document.getElementById("anzahl_urlaubstage");
-var anzahl_arbeitstage = document.getElementById("anzahl_arbeitstage");
+//var anzahl_arbeitstage = document.getElementById("anzahl_arbeitstage");
 var anzahl_bonuspunkte = document.getElementById("anzahl_bonuspunkte");
 
 
@@ -41,7 +41,7 @@ var anzahl_bonuspunkte = document.getElementById("anzahl_bonuspunkte");
 //result var for output
 var result = document.getElementById("result")
 var result_sum_anz_utage= document.getElementById("sum_anz_utage");
-var result_anzahl_arbeitstage = document.getElementById("sum_anz_atage");
+//var result_anzahl_arbeitstage = document.getElementById("sum_anz_atage");
 var result_anzahl_bonuspunkte = document.getElementById("sum_anz_bpkt");
 var result_sum_days = document.getElementById("sum_days");
 var result_sum_price = document.getElementById("sum_price");
@@ -52,7 +52,7 @@ console.log(result_sum_days);
 //add eventlistener
 saison.addEventListener("change",berechne_preis);
 intern_extern.addEventListener("change",berechne_preis);
-anzahl_arbeitstage.addEventListener("input", berechne_preis);
+//anzahl_arbeitstage.addEventListener("input", berechne_preis);
 anzahl_urlaubstage.addEventListener("input", berechne_preis);
 anzahl_bonuspunkte.addEventListener("input", berechne_preis);
 
@@ -69,7 +69,7 @@ function add_Bis(){
         console.log("test kat feature")
         document.getElementById("Bis").value =  document.getElementById("Von").value
       
-        document.getElementById("res_tage").innerHTML = "Deine Anzahl an Tagen beträgt: 0" 
+        document.getElementById("res_tage").innerHTML = "Deine Anzahl an Übernachtungen beträgt: 0" 
 
 
     }
@@ -112,7 +112,7 @@ if(!isNaN(new Date(document.getElementById("Von").value)) && !isNaN(new Date(doc
     console.log( (abreise - anreise)/1000/60/60/24) 
 var res_tage = (abreise - anreise)/1000/60/60/24;
 
-document.getElementById("res_tage").innerHTML = "Deine Anzahl an Tagen beträgt: " + res_tage
+document.getElementById("res_tage").innerHTML = "Deine Anzahl an Übernachtungen beträgt: " + res_tage
 
 }
 
@@ -129,17 +129,18 @@ var tmp_index=csv_preismodell.saison.indexOf(saison.value)
 
 var preis_urlaubstage = (csv_preismodell[intern_extern.value][tmp_index] )*anzahl_urlaubstage.value
 
-var preis_arbeitstage = (csv_preismodell.arbeitstarif[tmp_index] )*anzahl_arbeitstage.value
+//var preis_arbeitstage = (csv_preismodell.arbeitstarif[tmp_index] )*anzahl_arbeitstage.value
+//var add = preis_arbeitstage + preis_urlaubstage - 20*anzahl_bonuspunkte.value
 
 
-var add = preis_arbeitstage + preis_urlaubstage - 20*anzahl_bonuspunkte.value
+var add =  preis_urlaubstage - 20*anzahl_bonuspunkte.value
 
 
 
 if(add>0){
 
     document.getElementById("sum_anz_utage").innerHTML = preis_urlaubstage;
-    document.getElementById("sum_anz_atage").innerHTML = preis_arbeitstage;
+   // document.getElementById("sum_anz_atage").innerHTML = preis_arbeitstage;
     document.getElementById("sum_anz_bpkt").innerHTML = (-1)*20*anzahl_bonuspunkte.value;
     document.getElementById("sum_days").innerHTML =  (parseInt(anzahl_urlaubstage.value)+parseInt(anzahl_arbeitstage.value));
     document.getElementById("sum_price").value = add;
@@ -155,7 +156,7 @@ result.innerHTML = "Die Summe aus Arbeits- und Urlaubstagen beträgt " + add + "
     document.getElementById("sum_anz_bpkt").innerHTML = (-1)*20*anzahl_bonuspunkte.value;
     document.getElementById("sum_price").innerHTML = 0;
     document.getElementById("sum_anz_utage").innerHTML = 0;
-    document.getElementById("sum_anz_atage").innerHTML = 0;
+   // document.getElementById("sum_anz_atage").innerHTML = 0;
     document.getElementById("sum_days").innerHTML = 0;
 
 
